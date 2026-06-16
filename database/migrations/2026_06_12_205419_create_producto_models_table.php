80156ae5414103883d077e07c6b67a47fb4f5a54
Vladimirdeus <acostae139@gmail.com>
2026-06-16 17:50:39 -0600

    Carga de proyecti

---
+++ database/migrations/2026_06_12_205419_create_producto_models_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('producto_models', function (Blueprint $table) {
+            $table->id();
+
+        $table->string('codigo')->unique();
+
+        $table->string('descripcion');
+
+        $table->decimal('precio', 10, 2);
+
+        $table->integer('cantidad')->default(0);
+
+        $table->timestamps();
+
+        $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('producto_models');
+    }
+};
