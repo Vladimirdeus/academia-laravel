dcc8450f49d527fe59bbccd3f6043ec258f34c0f
Vladimirdeus <acostae139@gmail.com>
2026-06-16 17:42:10 -0600

    cargar el proyecto

---
+++ app/Http/Controllers/AprendizController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AprendizController extends Controller
+{
+    public function saludo(){
+        return "Hola, soy un aprendiz de Laravel";
+
+    }
+    //
+}
