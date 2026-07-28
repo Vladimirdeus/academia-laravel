9c200791ba1ff7ea4a4bf25f133126c6d8837a41
Vladimirdeus <acostae139@gmail.com>
2026-07-27 21:12:14 -0600

    Refactor Composer autoloading and error handling
    - Updated autoload.php to throw RuntimeException instead of triggering an error.
    - Changed Composer autoloader class and static initializer names for consistency.
    - Added selfDir property and method in InstalledVersions for better path handling.
    - Updated autoload_classmap.php to include BackendController.
    - Refactored autoload_real.php and autoload_static.php to reflect new class names.
    - Updated installed.php to reflect the new version and reference for laravel/laravel.
    - Improved platform_check.php to throw RuntimeException for platform issues.

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'laravel/laravel',
-        'pretty_version' => 'dev-develop',
-        'version' => 'dev-develop',
-        'reference' => '412ecb3e42686996ada0857b7981fae52b55f2e2',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'f987894e829da1d029d402468ae8f0a26558c797',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -476,9 +476,9 @@
             'dev_requirement' => false,
         ),
         'laravel/laravel' => array(
-            'pretty_version' => 'dev-develop',
-            'version' => 'dev-develop',
-            'reference' => '412ecb3e42686996ada0857b7981fae52b55f2e2',
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'f987894e829da1d029d402468ae8f0a26558c797',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
